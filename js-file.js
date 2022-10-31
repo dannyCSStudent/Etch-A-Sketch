a35d755f0eb2846aafa0eadcf47bf910e25cfbc2
Dan Villegas <danv8605@gmail.com>
2022-10-31 16:23:47 -0600

    Added js to create grid
    Wired the clear button to clear the container
    if it already has content. Then a function
    called createGrid is called and sets the grid with
    the size that will be used, 16 x 16 at this moment.
    A listener fo each div that was
    create is also set.

--- js-file.js
+++ js-file.js
@@ -1,19 +1,88 @@
+const container = document.querySelector('#second-column');
+const gridLenght = 16;
+
+// creates the divs in a flexbox container
+// after calculating the size of the div's to
+// fit perfectly, will build divs and wrap around.
 function createGrid(size) {
     for (let i = 0; i < (size ** 2); i++) {
-        const container = document.querySelector('#container');
+        
         const content = document.createElement('div');
         content.classList.add('content');
-        content.style.border = "1px solid black";
         content.style.flexBasis = `${100 / size}%`;
-        content.textContent = i;
+        content.style.height = `${100 / size}%`;
         container.appendChild(content);
+    }
+}
+
+function clear(){
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
     }
 }
 
 
 
+createGrid(gridLenght);
+// ----------------------Buttons---------------------------------
 
-// ----------------------Demarc----------------------------------
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        switch (button.id) {
+            case "clear":
+                clear();
+                createGrid(gridLenght);
+                
+                break
+            case "black-ink":
+                break
+            case "random-ink":
+                break
+            case "rainbow-ink":
+                break
+            case "erase":
+                break
+                                                             
+        }
+    })
+});
 
-let s = 100;
-// createGrid(s);
+// ----------------mouseover listener-------------------------------------
+
+container.addEventListener("mouseover", function(e) {
+    if (e.target.matches(".content")) {
+        e.target.style.backgroundColor = "rgb(0, 0, 0)";
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
