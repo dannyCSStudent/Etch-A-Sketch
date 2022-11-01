dbc89ac56bd99f2ca3f5f7510bd7ae22bcd487c4
Dan Villegas <danv8605@gmail.com>
2022-11-01 07:03:27 -0600

    Added the range slider
    Added js to get the value from the slider
    and evertime the clear button is clicked
    the side of the squares in the divs will change.

--- js-file.js
+++ js-file.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('#second-column');
-const gridLenght = 16;
+
+const gridLenght = document.getElementById("slider").value;
 
 // creates the divs in a flexbox container
 // after calculating the size of the div's to
@@ -21,6 +22,14 @@ function clear(){
     }
 }
 
+function listener() {
+    container.addEventListener("mouseover", function(e) {
+        if (e.target.matches(".content")) {
+            e.target.style.backgroundColor = "rgb(0, 0, 0)";
+        }
+    })
+}
+
 
 
 createGrid(gridLenght);
@@ -32,16 +41,46 @@ buttons.forEach((button) => {
         switch (button.id) {
             case "clear":
                 clear();
+                const gridLenght = document.getElementById("slider").value;
                 createGrid(gridLenght);
-                
+                listener();
                 break
             case "black-ink":
+                listener();
                 break
             case "random-ink":
+                let r = Math.random() * 255;
+                let g = Math.random() * 255;
+                let b = Math.random() * 255;
+                container.addEventListener("mouseover", function(e) {
+                    if (e.target.matches(".content")) {
+                    
+                        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+                    }
+                })
                 break
             case "rainbow-ink":
+                const rainbowColorList = ["red"
+                , "orange"
+                , "green"
+                , "blue"
+                , "indigo"
+                , "violet"];
+
+                container.addEventListener("mouseover", function(e) {
+                    if (e.target.matches(".content")) {
+                        e.target.style.backgroundColor =
+                        rainbowColorList[Math.floor(Math.random() *
+                        rainbowColorList.length)];
+                    }
+                })          
                 break
             case "erase":
+                container.addEventListener("mouseover", function(e) {
+                    if (e.target.matches(".content")) {
+                        e.target.style.backgroundColor = "white";
+                    }
+                })
                 break
                                                              
         }
